2e5a53433ab2101ca9700a381f26c210297200d0
Abdul Aliyev <aaa7c4@gmail.com>
2020-10-08 17:19:36 -0400

    Add new bus adding handling

--- controllers/realtimeBusInfoController.js
+++ controllers/realtimeBusInfoController.js
@@ -1,14 +1,17 @@
 const axios = require("axios");
 
-async function realtimeBusInfo(io, socket, callback) {
-  // console.log("in realtimeBusInfo and message = ", message);
-
-  // socket.emit("message", "Message for one socket");
-  const url = `https://api.wmata.com/NextBusService.svc/json/jPredictions?api_key=${process.env.WMATA_API_KEY}&StopID=1003419`;
+async function realtimeBusInfo(io, socket, stopIDs, callback) {
+  const buildUrl = (stopID) =>
+    `https://api.wmata.com/NextBusService.svc/json/jPredictions?api_key=${process.env.WMATA_API_KEY}&StopID=${stopID}`;
 
   try {
-    const response = await axios.get(url);
-    callback(response.data);
+    const requests = stopIDs.map((id) => axios.get(buildUrl(id)));
+
+    const response = await axios
+      .all(requests)
+      .then(axios.spread((...responses) => responses.map((r) => r.data)));
+
+    callback(response);
   } catch (error) {
     callback(error);
   }
